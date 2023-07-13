c496f758e951d264038512cb1ed50fc1e9e8284b
Burcu <122625978+BurcuBulakBozkurt@users.noreply.github.com>
2023-07-13 14:57:58 +0200

    ft_strlcat.c

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -12,42 +12,39 @@
 
 #include "libft.h"
 
-size_t	ft_strlcat(char *dst, const char *src, size_t size)
+size_t  ft_strlcat(char *dst, const char *src, size_t size)
 {
-	char	*str;
-	size_t	len_dst;
-	size_t	res;
-	size_t	len_src;
-	size_t	i;
+    size_t  len_dst;
+    size_t  res;
+    size_t  len_src;
+    size_t  i;
 
-	str = (char *)src;
-	len_dst = ft_strlen(dst);
-	len_src = ft_strlen(src);
-	res = 0;
-	i = 0;
-	if (size > len_dst)
-		res = len_src + len_dst;
-	else
-		res = len_src + size;
-	while (str[i] && (len_dst + 1) < size)
-	{
-		dst[len_dst] = str[i];
-		len_dst++;
-		i++;
-	}
-	dst[len_dst] = '\0';
-	return (res);
+    len_dst = ft_strlen(dst);
+    len_src = ft_strlen(src);
+    res = 0;
+    i = 0;
+    if (size > len_dst)
+        res = len_src + len_dst;
+    else
+        res = len_src + size;
+    while (src[i] != '\0' && (len_dst + 1) < size)
+    {
+        dst[len_dst] = src[i];
+        len_dst++;
+        i++;
+    }
+    dst[len_dst] = '\0';
+    return (res);
 }
 
-/*int main(void)
+/*
+int main(void)
 {
-    char dest[20] = "Hello";
-    char src[] = "World";
-    
-    size_t new_len = strlcat(dest, src, sizeof(dest));
-    
-    printf("Concatenated string: %s\n", dest);
-    printf("New length: %zu\n", new_len);
-    
-    return 0;
+    char    src[] = "World";
+    char    dst[15] = "Hello";
+
+    printf("strlcat: %ld \n", ft_strlcat(dst,src,7));
+    printf("Copied string: %s\n", dst);
+
+    return (0);
 }*/
