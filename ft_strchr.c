0c3ca63f4a7894478bfe529f583cd5a3855c030e
Burcu <122625978+BurcuBulakBozkurt@users.noreply.github.com>
2023-07-19 01:05:47 +0200

    ft_strchr.c

--- ft_strchr.c
+++ ft_strchr.c
@@ -15,7 +15,7 @@
 char	*ft_strchr(const char *s, int c)
 {
 	while ((unsigned char)*s != (unsigned char)c && *s != '\0')
-		s ++;
+		s++;
 	if (*s == (char)c)
 		return ((char *)s);
 	return (NULL);
